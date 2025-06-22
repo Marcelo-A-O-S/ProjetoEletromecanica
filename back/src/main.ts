2fcaaebc81e017426b4bc7996a8ac4132fa61e95
Marcelo-A-O-S <marceloaugustooliveirasoares@gmail.com>
2025-06-22 18:12:02 -0300

    Atualizando arquivo principal para deploy do projeto

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,33 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import serverlessExpress from '@vendia/serverless-express';
-import { Handler } from 'aws-lambda';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express'
 
-let cachedServer: Handler;
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  await app.init();
   if (process.env.NODE_ENV !== 'production') {
     await app.listen(process.env.PORT ?? 3000);
-  }else {
-    await app.init();
   }
   return app;
 }
 bootstrap();
-
- const handler: Handler = async (event, context, callback) => {
-  if (!cachedServer) {
-    const app = await bootstrap();
-    const expressApp = app.getHttpAdapter().getInstance();
-    cachedServer = serverlessExpress({ app: expressApp });
-  }
-  return cachedServer(event, context, callback);
-};
-export default handler;
- 
