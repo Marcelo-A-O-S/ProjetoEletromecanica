04a37b15472215a67d2bc8bd57ea4543395ea4c8
Marcelo-A-O-S <marceloaugustooliveirasoares@gmail.com>
2025-06-22 14:46:10 -0300

    Atualizando configuração do vercel.json

--- back/dist/main.js
+++ back/dist/main.js
@@ -1,8 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const serverless_express_1 = require("@vendia/serverless-express");
+const serverless_express_1 = __importDefault(require("@vendia/serverless-express"));
 let cachedServer;
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
