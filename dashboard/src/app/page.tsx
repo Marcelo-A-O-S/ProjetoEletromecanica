3d916b00bc82e9b6db1dda6d383c12327c768a73
Marcelo-A-O-S <marceloaugustooliveirasoares@gmail.com>
2025-06-22 19:04:46 -0300

    Atualizando metadata da homepage

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -1,6 +1,6 @@
 
 import type { Metadata } from "next";
-import {  HomeClient} from "@/components/pages/home-client";
+import { HomeClient } from "@/components/pages/home-client";
 export const metadata: Metadata = {
   title: {
     default: "Projeto Eletromecânica 2024",
@@ -8,9 +8,46 @@ export const metadata: Metadata = {
   },
   description: "Projeto desenvolvido para disciplina de projetos do colégio Escola Estadual Professor Gastão Valle.",
   authors: [{
-    name: 'Marcelo Augusto de Oliveira Soares'
-  }]
+    name: 'Marcelo Augusto de Oliveira Soares', url: 'https://github.com/Marcelo-A-O-S'
+  }],
+  generator: 'Next.js',
+  robots: 'index, follow',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+  },
+  referrer: 'origin-when-cross-origin',
+  keywords:
+    [
+      'escola estadual professor gastão valle',
+      'noturno',
+      'home',
+    ],
+  alternates: {
+    canonical: "https://projeto-eletromecanica-2024.vercel.app/",
+    languages: {
+      pt: "https://projeto-eletromecanica-2024.vercel.app/",
+    }
+  },
+  openGraph: {
+    siteName: "Projeto Eletromecânica 2024",
+    locale: 'pt_BR',
+    title: "Projeto Eletromecânica 2024",
+    url: "https://projeto-eletromecanica-2024.vercel.app/",
+    description: "Projeto desenvolvido para disciplina de projetos do colégio Escola Estadual Professor Gastão Valle.",
+    images: [
+      {
+        url: "https://projeto-eletromecanica-2024.vercel.app/1.jpg",
+        width: 500,
+        height: 500,
+        alt: "Projeto de Eletromecânica",
+      },
+    ],
+    type: 'website',
+  },
+  category: 'education',
+  creator: "Marcelo Augusto",
 };
 export default function Home() {
-  return <HomeClient/>
+  return <HomeClient />
 }
