ff3fadf280294d95d5e583695c4bfb7895f0e5f6
Marcelo-A-O-S <marceloaugustooliveirasoares@gmail.com>
2025-06-26 15:34:18 -0300

    Atualizando funcao de atualizacao de configuracao

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from "next";
 import { HomeClient } from "@/components/pages/home-client";
 export const metadata: Metadata = {
   title: {
-    default: "Projeto Eletromecânica 2024",
-    template: "%s | Projeto Eletromecânica 2024",
+    default: "Projeto Eletromecânica 2025",
+    template: "%s | Projeto Eletromecânica 2025",
   },
   description: "Projeto desenvolvido para disciplina de projetos do colégio Escola Estadual Professor Gastão Valle.",
   authors: [{
